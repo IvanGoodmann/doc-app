f6bd899b156b055c90dc062a5666c11be0b367fd
Ivan <xdistil@gmail.com>
2020-02-06 20:35:17 +0400

    Базовая верстка проекта. Добавлены пункты меню.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,121 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <v-app id="inspire">
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                clipped
+                :expand-on-hover="expandOnHover"
+                :mini-variant="miniVariant"
+                absolute
+                dark
+        >
+            <v-list
+                    dense
+                    nav
+                    class=""
+            >
+                <v-list-item
+                        link
+                        v-for="(item, idx) in menuItems"
+                        :key="idx"
+                        class="pl-4 pr-3"
+                >
+                    <v-list-item-action
+                        class="mr-4"
+                    >
+                        <v-icon>{{item.icon}}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{item.title}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar
+                app
+                clipped-left
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <v-toolbar-title>Документация</v-toolbar-title>
+        </v-app-bar>
+
+        <v-content>
+            <v-container
+                    class="fill-height"
+                    fluid
+            >
+                <v-row
+                        align="center"
+                        justify="center"
+                >
+                    <v-col class="shrink">
+
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+
+        <v-footer app>
+            <span>&copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
+    </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null,
+    menuItems: [
+      {
+        icon: 'mdi-view-dashboard',
+        title: 'Верстка',
+        link: ''
+      },
+      {
+        icon: 'mdi-folder-upload',
+        title: 'Заливка креативов',
+        link: ''
+      },
+      {
+        icon: 'mdi-numeric-2-box-multiple-outline',
+        title: 'Second Page',
+        link: ''
+      },
+      {
+        icon: 'mdi-numeric-3-box-multiple-outline',
+        title: 'Success Page',
+        link: ''
+      },
+      {
+        icon: 'mdi-file-video',
+        title: 'Video',
+        link: ''
+      },
+      {
+        icon: 'mdi-keyboard',
+        title: 'Работа с формами',
+        link: ''
+      },
+      {
+        icon: 'mdi-information-outline',
+        title: 'Общие вопросы',
+        link: ''
+      }
+    ],
+    miniVariant: true,
+    expandOnHover: true
+  }),
+  created () {
+    this.$vuetify.theme.dark = true
   }
 }
+
+</script>
+
+<style lang="scss">
+    @import './assets/scss/main';
 </style>
